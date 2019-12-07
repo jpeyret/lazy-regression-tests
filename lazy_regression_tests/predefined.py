f47c95da522074dac9808dfb41a2ebaa7c3fdd24
JL Peyret <jpeyret@gmail.com>
2019-12-06 22:16:23 -0800

    "just a backup, use it sparingly on main codelines"

--- lazy_regression_tests/predefined.py
+++ lazy_regression_tests/predefined.py
@@ -1,6 +1,32 @@
 import json
 
 
+def cpdb(**kwds: "Any") -> bool:  # pragma: no cover
+    if cpdb.enabled == "once":
+        cpdb.enabled = False  # type : ignore
+        return True
+    return cpdb.enabled  # type : ignore
+
+
+cpdb.enabled = False  # type : ignore
+
+
+def rpdb() -> bool:  # pragma: no cover
+    try:
+        from django.core.cache import cache
+    except (Exception,) as e:
+        cache = {}
+    import sys
+
+    in_celery = sys.argv[0].endswith("celery") and "worker" in sys.argv
+    if in_celery:
+        return False
+    return bool(cache.get("rpdb_enabled") or getattr("rpdb", "enabled", False))
+
+
+rpdb.enabled = False  # type : ignore
+
+
 from lazy_regression_tests.core2 import LazyCheckerOptions2 as LazyCheckerOptions
 
 
