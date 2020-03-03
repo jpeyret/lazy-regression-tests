fd9548771f5e3f2ce448c7f78f3041888154bb84
JL Peyret <jpeyret@gmail.com>
2020-03-02 18:00:33 -0800

    minor tweaks

--- lazy_regression_tests/__init__.py
+++ lazy_regression_tests/__init__.py
@@ -2,7 +2,7 @@
 
 # -__author__ = """JL Peyret"""
 # -__email__ = "jpeyret@gmail.com"
-# -__version__ = "__version__ = '0.2.3'"
+# -__version__ = "__version__ = '__version__ = '0.2.3''"
 
 
 from .core import LazyMixin, ValidationManager
