dabe63be133a9c5bc7ffc9815b97c64da0c2ad83
JL Peyret <jpeyret@gmail.com>
2020-03-02 17:46:52 -0800

    version tweak

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jpeyret/lazy-regression-tests",
-    version="0.5.0",
+    version="0.2.3",
     zip_safe=False,
 )
